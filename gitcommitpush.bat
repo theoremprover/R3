5e2910116886cb4d7591a77060d042202530d675
Reitmeier <reitmeier@validas.de>
2022-02-08 12:34:15 +0100

    asd

---
+++ gitcommitpush.bat
@@ -0,0 +1,21 @@
+@echo off
+
+SET WORKDIR=%~dp0
+cd %WORKDIR%
+
+where plink.exe > tmpFile
+set /p GIT_SSH= < tmpFile
+del tmpFile
+
+git commit -a -m "No Message"
+if errorlevel 2 goto :err
+git pull origin master
+if errorlevel 1 goto :err
+git push origin master
+if errorlevel 1 goto :err
+echo OK
+exit /B 0
+
+:err
+pause
+exit /B 1
