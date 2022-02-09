719af7dfd3bb511fbaca07217460a88300c8e784
Reitmeier <reitmeier@validas.de>
2022-02-09 09:13:05 +0100

    dsad

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -1,3 +1,16 @@
-module Parsing where
+module Parsing (
+	parseFile_CLanguage)
+	where
 
 import Language.C
+
+import AST
+
+parseFile_CLanguage :: String -> FilePath -> IO (Either String AST)
+parseFile_CLanguage gcc filepath = do
+	readFile filepath >>= parseCFile (newGCC gcc) (Just filepath) >>= \case
+		Left parseerror   -> return $ Left $ show parseerror
+		Right ctranslunit -> return $ languagecToAST ctranslunit
+
+languagecToAST :: CTranslUnit -> AST
+languagecToAST ctranslunit = return $
