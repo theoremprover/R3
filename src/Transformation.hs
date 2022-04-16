508607563413cacd016413b0fc0d044dbbb32fc1
theoremprover <none@none.com>
2022-04-16 23:33:38 +0200

    123

--- src/Transformation.hs
+++ src/Transformation.hs
@@ -63,76 +63,56 @@ int** pp = &p;
 -}
 
 transformAST :: TranslUnit → R3 TranslUnit
-transformAST ast = do
-	machinespec <- gets machineSpecR3
-	elimConstructs machinespec ast >>= elimSideEffects machinespec
+transformAST ast = return ast >>=
+	elimConstructs >>= elimSideEffects
 
 newIdent :: String → R3 Ident
 newIdent prefix = do
 	i <- getNewNameCnt 
 	return $ Ident (prefix ++ "$" ++ show i) i introLoc
 
-elimConstructs :: MachineSpec → TranslUnit → R3 TranslUnit
-elimConstructs MachineSpec{..} ast = transformBiM stmt_rules ast
+elimConstructs :: TranslUnit → R3 TranslUnit
+elimConstructs ast = transformBiM stmt_rules ast
 	where
 
- 	cboolTy :: ZType
-	cboolTy = ZInt intSize False
+	stmt_rules :: Stmt -> R3 Stmt
 
-	indexTy :: ZType
-	indexTy = ZInt intSize True
-
- 	stmt_rules :: Stmt -> R3 Stmt
-
-  	stmt_rules Switch{..} = do
-  		val_ident <- newIdent "switch_val"
-  		let
-  			val_ty = typeE condS
-  			val_var = Var val_ident val_ty introLoc
-  			Compound _ stmts loc = bodyS
-  		return $ Compound True [
+	stmt_rules Switch{..} = do
+		MachineSpec{..} <- gets machineSpecR3
+		val_ident <- newIdent "switch_val"
+		let
+			val_ty = typeE condS
+			val_var = Var val_ident val_ty introLoc
+			Compound _ stmts loc = bodyS
+		return $ Compound True [
 			val_ident ∶∶ val_ty,
 			val_var ≔ condS,
-  			search_cases val_var stmts ] loc
+  			search_cases (ZInt intSize False) val_var stmts ] loc
+
   		where
-		search_cases val_var [] = emptyStmt
-		search_cases val_var (Default _ : stmts) = Compound False stmts introLoc
-		search_cases val_var (Case {..} : stmts) = 𝗂𝖿 cond cases_filtered_out till_case where
-			cond = Binary Equals val_var condS cboolTy locS
+
+		search_cases _ val_var [] = emptyStmt
+		search_cases _ val_var (Default _ : stmts) = Compound False stmts introLoc
+		search_cases cboolty val_var (Case {..} : stmts) = 𝗂𝖿 cond cases_filtered_out till_case
+			where
+			cond                = Binary Equals val_var condS cboolty locS
 			isnocase (Case _ _) = False
 			isnocase _          = True
-			cases_filtered_out = Compound False (filter isnocase stmts) introLoc
-			till_case = search_cases val_var (dropWhile isnocase stmts)
-		search_cases val_var (_ : stmts) = search_cases val_var stmts
+			cases_filtered_out  = Compound False (filter isnocase stmts) introLoc
+			till_case           = search_cases cboolty val_var (dropWhile isnocase stmts)
+		search_cases cboolty val_var (_ : stmts) = search_cases cboolty val_var stmts
 
 	stmt_rules other = return other
 
-elimSideEffects :: MachineSpec → TranslUnit → R3 TranslUnit
-elimSideEffects MachineSpec{..} ast = do
-	liftIO $ mapM_ print [ show loc ++ " : " ++ show (pretty stm) |
-		Compound _ stmts _ <- universeBi ast,
-		stm@(ExprStmt expr loc) <- stmts,
-		Unary op _ _ _ :: Expr <- children expr ]
---		op `elem` [PreInc,PostInc,PreDec,PostDec] ]
-	return ast
---	ast' <- transformBiM expr_rules ast
+elimSideEffects :: TranslUnit → R3 TranslUnit
+elimSideEffects ast = transformBiM elim_rules ast
+	where
+	elim_rules :: Stmt -> R3 Stmt
+	elim_rules stmt = case [ stmts |
+		Unary op expr ty _ <- universeBi stmt,
+		op `elem` [PreInc,PostInc,PreDec,PostDec],
+		]
+		of
+		[] -> return stmt
+		stmts -> return $ Compound False stmts introLoc
 
-
-{-
-asta = C [ExpStm (V 1), C [ExpStm (I (V 9)),ExpStm (V 3)]]
-test = mapM_ print [ (show stm) |
-	stm :: Stm <- universeBi asta,
-	True ]
--}
-
-{-
-infer_expr
-	Assign {
-		lexprE = Var {identE = Ident {nameIdent = "a", idIdent = 97, locIdent = switchtest.c, line 3, col 9, len 1},
-		typeE = Just (DirectType (TyIntegral int) (TypeQuals {constant = False, volatile = False, restrict = False, atomic = False, nullable = False, nonnull = False, clrdonly = False, clwronly = False}) [],[]),
-		locE = switchtest.c, line 3, col 9, len 1},
-		exprE = Constant {constE = IntConst 0, typeE = Just (DirectType (TyIntegral int) (TypeQuals {constant = False, volatile = False, restrict = False, atomic = False, nullable = False, nonnull = False, clrdonly = False, clwronly = False}) [],[]), locE = switchtest.c, line 3, col 13, len 1},
-		typeE = Just (DirectType (TyIntegral int) (TypeQuals {constant = False, volatile = False, restrict = False, atomic = False, nullable = False, nonnull = False, clrdonly = False, clwronly = False}) [],[]), locE = switchtest.c, line 3, col 9, len 1}
-		not implemented
-
--}
